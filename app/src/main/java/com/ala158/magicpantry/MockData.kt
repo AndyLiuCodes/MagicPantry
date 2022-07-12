91cb0c7ab0f167960f6556343db5a246df62314d
Tyler Trinh <tylervtrinh@gmail.com>
2022-07-12 01:59:51 -0700

    feat: add review ingredients page to home page temporarily

---
+++ app/src/main/java/com/ala158/magicpantry/MockData.kt
@@ -0,0 +1,13 @@
+package com.ala158.magicpantry
+
+import com.ala158.magicpantry.data.Ingredient
+
+object MockData {
+    val ingredients = arrayListOf(
+        Ingredient(0, "Banana", 3, "unit", 0.50),
+        Ingredient(1, "Apple", 1, "unit", 0.20),
+        Ingredient(2, "Linguine", 300, "grams", 5.00),
+        Ingredient(3, "Tomato Sauce", 1, "litre", 10.00),
+        Ingredient(4, "Sugar", 100, "grams", 3.00),
+    )
+}
---
+++ app/src/main/java/com/ala158/magicpantry/arrayAdapter/IngredientsArrayAdapter.kt
@@ -0,0 +1,44 @@
+package com.ala158.magicpantry.arrayAdapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.ala158.magicpantry.R
+import com.ala158.magicpantry.data.Ingredient
+
+class IngredientsArrayAdapter(
+    private val context: Context,
+    private var ingredients: List<Ingredient>
+) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return ingredients.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return ingredients[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View = View.inflate(context, R.layout.list_item_ingredient, null)
+        val nameAmountView = view.findViewById<TextView>(R.id.ingredientNameAmount)
+        val priceUnitView = view.findViewById<TextView>(R.id.ingredientPriceUnit)
+
+        val ingredient = ingredients[position]
+        nameAmountView.text = "${ingredient.amount}x ${ingredient.name}"
+        val price = String.format("$%.2f", ingredient.price)
+        priceUnitView.text = "$price/${ingredient.unit}"
+        return view
+    }
+
+    fun replace(newIngredients: List<Ingredient>) {
+        ingredients = newIngredients.toList()
+    }
+
+}
