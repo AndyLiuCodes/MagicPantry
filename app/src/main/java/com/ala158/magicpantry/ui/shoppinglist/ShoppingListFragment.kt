332aa1a219e6196efef808052b825dae9525312d
Andy Liu <andyliu5898@gmail.com>
2022-07-24 01:16:46 -0700

    Added Shopping List UI - Still needs backend logic

--- app/src/main/java/com/ala158/magicpantry/ui/shoppinglist/ShoppingListFragment.kt
+++ app/src/main/java/com/ala158/magicpantry/ui/shoppinglist/ShoppingListFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.ala158.magicpantry.MockData
 import com.ala158.magicpantry.R
+import com.ala158.magicpantry.arrayAdapter.ShoppingListArrayAdapter
 
 class ShoppingListFragment : Fragment() {
     private lateinit var shoppingListViewModel: ShoppingListViewModel
+    private lateinit var shoppingListListView: ListView
+    private lateinit var shoppingListArrayAdapter: ShoppingListArrayAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +24,11 @@ class ShoppingListFragment : Fragment() {
         shoppingListViewModel = ViewModelProvider(this).get(ShoppingListViewModel::class.java)
 
         val view = inflater.inflate(R.layout.fragment_shopping_list, container, false)
+        shoppingListListView = view.findViewById(R.id.listview_shopping_list_items)
 
-        val textView: TextView = view.findViewById(R.id.text_shopping_list)
-        shoppingListViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        shoppingListArrayAdapter = ShoppingListArrayAdapter(requireActivity(), MockData.shoppingList)
+        shoppingListListView.adapter = shoppingListArrayAdapter
+
         return view
     }
 }
