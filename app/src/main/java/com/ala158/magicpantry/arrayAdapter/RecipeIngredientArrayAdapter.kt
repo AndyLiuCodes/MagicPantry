3f97a53445299d9a7fb4d9309157888cacb226df
Jason Esquilona <jesquilo@sfu.ca>
2022-07-28 22:13:46 -0700

    Created List item for recipes Ingredients, still need to do some testing of list items

--- app/src/main/java/com/ala158/magicpantry/arrayAdapter/RecipeIngredientArrayAdapter.kt
+++ app/src/main/java/com/ala158/magicpantry/arrayAdapter/RecipeIngredientArrayAdapter.kt
@@ -1,4 +1,54 @@
 package com.ala158.magicpantry.arrayAdapter
 
-class RecipeIngredientArrayAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.ala158.magicpantry.R
+import com.ala158.magicpantry.data.Ingredient
+import com.ala158.magicpantry.data.RecipeWithIngredients
+
+class RecipeIngredientArrayAdapter(private val context: Context, private var recipeIngredients:List<Ingredient>, private var allIngredients:List<Ingredient>): BaseAdapter() {
+    override fun getCount(): Int {
+        return recipeIngredients.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return recipeIngredients[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return recipeIngredients[position].ingredientId
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = View.inflate(context, R.layout.list_item_recipe_ingredients, null)
+        val ingredientAmount: TextView = view.findViewById<TextView>(R.id.recipe_ingredient_amount)
+        val ingredientName: TextView = view.findViewById<TextView>(R.id.recipe_ingredient_name)
+        val alertSymbol: ImageView = view.findViewById<ImageView>(R.id.recipeIngredient_low_stock_icon)
+        ingredientAmount.text = "${recipeIngredients[position].amount} ${recipeIngredients[position].unit}"
+        ingredientName.text = "${recipeIngredients[position].name}"
+        val findIngredient = allIngredients.find {it.name == recipeIngredients[position].name}
+        if (findIngredient != null) {
+            if(findIngredient.amount < recipeIngredients[position].amount){
+                alertSymbol.visibility = View.VISIBLE
+            }else{
+                alertSymbol.visibility = View.INVISIBLE
+            }
+        }
+        else{
+            alertSymbol.visibility = View.VISIBLE
+        }
+        return view
+    }
+
+    fun replaceRecipeIngredients(newRecipes:List<Ingredient>){
+        recipeIngredients = newRecipes.toList()
+    }
+
+    fun replaceAllIngredients(newIngredients:List<Ingredient>){
+        allIngredients = newIngredients.toList()
+    }
 }
--- app/src/main/java/com/ala158/magicpantry/ui/singlerecipe/SingleRecipeActivity.kt
+++ app/src/main/java/com/ala158/magicpantry/ui/singlerecipe/SingleRecipeActivity.kt
@@ -2,21 +2,29 @@ package com.ala158.magicpantry.ui.singlerecipe
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.ala158.magicpantry.R
 import com.ala158.magicpantry.Util
 import com.ala158.magicpantry.arrayAdapter.RecipeIngredientArrayAdapter
+import com.ala158.magicpantry.arrayAdapter.RecipeListArrayAdapter
 import com.ala158.magicpantry.data.RecipeWithIngredients
+import com.ala158.magicpantry.viewModel.IngredientViewModel
 import com.ala158.magicpantry.viewModel.RecipeViewModel
 
 class SingleRecipeActivity : AppCompatActivity() {
 
     private lateinit var recipeViewModel: RecipeViewModel
+    private lateinit var ingredientViewModel: IngredientViewModel
     private lateinit var recipeName:TextView
-    private lateinit var description:TextView
+    private lateinit var recipeDescription:TextView
     private lateinit var ingredientListView: ListView
+    private lateinit var cookingTimeView: TextView
+    private lateinit var numOfServingsView:TextView
+    private lateinit var isAbleToCookImageView: ImageView
+    private lateinit var isAbleToCookTextView: TextView
     private lateinit var editRecipeButton: Button
     private lateinit var addIngredientsButton: Button
     private lateinit var recipeIngredientArrayAdapter: RecipeIngredientArrayAdapter
@@ -29,24 +37,65 @@ class SingleRecipeActivity : AppCompatActivity() {
             RecipeViewModel::class.java,
             Util.DataType.RECIPE
         )
+        ingredientViewModel = Util.createViewModel(
+            this,
+            IngredientViewModel::class.java,
+            Util.DataType.INGREDIENT
+        )
         recipeName = findViewById(R.id.singleRecipeName)
+        recipeDescription = findViewById(R.id.recipe_description)
+        ingredientListView = findViewById(R.id.listview_all_recipe_ingredients)
+        cookingTimeView = findViewById(R.id.CookingTime)
+        numOfServingsView =findViewById(R.id.NoOfServings)
+        isAbleToCookImageView = findViewById(R.id.enoughIngredientsImage)
+        isAbleToCookTextView = findViewById(R.id.EnoughIngredientsText)
 
         val id = intent.getIntExtra("RECIPE_KEY",-1)
         val id2 = intent.getIntExtra("RECIPE_KEY_COOKABLE",-1)
-
+        recipeIngredientArrayAdapter = RecipeIngredientArrayAdapter(this, ArrayList(), ArrayList())
+        ingredientListView.adapter = recipeIngredientArrayAdapter
 
         recipeViewModel.allRecipes.observe(this) {
             recipeViewModel.updateCurrentCookable()
             if(id != -1) {
                 var recipeWithIngredients: RecipeWithIngredients = it[id]
                 recipeName.text = recipeWithIngredients.recipe.title
+                cookingTimeView.text = "${recipeWithIngredients.recipe.timeToCook}"
+                numOfServingsView.text = "${recipeWithIngredients.recipe.servings}"
+                if(recipeWithIngredients.recipe.numMissingIngredients == 0){
+                    isAbleToCookImageView.setImageResource(R.drawable.ic_baseline_check_box_24)
+                    isAbleToCookTextView.text = "Ready to cook!"
+                }
+                else{
+                    isAbleToCookImageView.setImageResource(R.drawable.low_stock)
+                    isAbleToCookTextView.text = "Missing ${recipeWithIngredients.recipe.numMissingIngredients} ingredients"
+                }
+                recipeIngredientArrayAdapter.replaceRecipeIngredients(it[id].ingredients)
+                recipeIngredientArrayAdapter.notifyDataSetChanged()
             }
         }
         recipeViewModel.cookableRecipes.observe(this){
             if(id2 != -1) {
-                var recipeWithIngredients: RecipeWithIngredients = it[id]
+                var recipeWithIngredients: RecipeWithIngredients = it[id2]
                 recipeName.text = recipeWithIngredients.recipe.title
+                cookingTimeView.text = "${recipeWithIngredients.recipe.timeToCook}"
+                numOfServingsView.text = "${recipeWithIngredients.recipe.servings}"
+                if(recipeWithIngredients.recipe.numMissingIngredients == 0){
+                    isAbleToCookImageView.setImageResource(R.drawable.ic_baseline_check_box_24)
+                    isAbleToCookTextView.text = "Ready to cook!"
+                }
+                else{
+                    isAbleToCookImageView.setImageResource(R.drawable.low_stock)
+                    isAbleToCookTextView.text = "Missing ${recipeWithIngredients.recipe.numMissingIngredients} ingredients"
+                }
+                recipeIngredientArrayAdapter.replaceRecipeIngredients(it[id2].ingredients)
+                recipeIngredientArrayAdapter.notifyDataSetChanged()
             }
+        }
+
+        ingredientViewModel.allIngredientsLiveData.observe(this){
+            recipeIngredientArrayAdapter.replaceAllIngredients(it)
+            recipeIngredientArrayAdapter.notifyDataSetChanged()
         }
     }
 }
